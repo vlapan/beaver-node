14458114debde77a8029ec4d68378dcb441b95e1
vlapan <vlapan@vlapan.com>
2014-08-11 23:41:43 +0400

    updates

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var argv = require(__dirname + '/lib/argv');
 var logger = require(__dirname + '/lib/logger');
 
-
 var platforms = require(path.normalize(__dirname + '/lib/platforms'));
 var platform = os.platform();
 if (argv.p) {
@@ -23,17 +22,86 @@ var config = require(__dirname + '/lib/configuration');
 
 var extensions = require(__dirname + '/lib/extensions');
 
-extensions.generate(function () {
-	if (argv.d) {
-		function daemonNote() {
-			logger.log('info', 'Daemon mode, watching config file "' + argv.i + '"!');
-		}
-		daemonNote();
+if (argv.i) {
+	extensions.generate(callback);
+} else if (argv.d) {
+	callback();
+}
 
-		fs.watchFile(path.resolve(argv.i), function (curr, prev) {
-			logger.log('info', 'config file, modification detected "' + argv.i + '"!');
-			config.reread();
-			extensions.generate(daemonNote);
-		});
+function callback() {
+	if (argv.d) {
+		if (argv.i) {
+			function fileDaemonNote() {
+				logger.log('info', 'Daemon mode, watching config file "' + argv.i + '"!');
+			}
+			fileDaemonNote();
+
+			fs.watchFile(path.resolve(argv.i), function (curr, prev) {
+				logger.log('info', 'config file, modification detected "' + argv.i + '"!');
+				config.readFile();
+				extensions.generate(daemonNote);
+			});
+		}
+
+		if (argv.sslPrefix) {
+			function serverDaemonNote() {
+				logger.log('info', 'Daemon mode, listening ' + argv.httpsPort + '!');
+			}
+			serverDaemonNote();
+
+			var express = require('express');
+			var https = require('https');
+			var app = express();
+
+			var basicAuth = require('basic-auth');
+			var pam = require('authenticate-pam');
+			var auth = function (req, res, next) {
+				function unauthorized(res) {
+					res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+					return res.status(401).end();
+				}
+
+				var user = basicAuth(req);
+
+				if (!user || !user.name || !user.pass) {
+					return unauthorized(res);
+				}
+
+				pam.authenticate(user.name, user.pass, function (err) {
+					if (err) {
+						logger.error(err);
+						return unauthorized(res);
+					} else {
+						logger.log('info', '"' + user.name + '" user authenticated!');
+						return next();
+					}
+				});
+			};
+
+			app.use(auth);
+
+			var bodyParser = require('body-parser');
+			app.use(bodyParser.urlencoded({
+				extended: false
+			})); // to support URL-encoded bodies
+
+			app.get('/', function (req, res) {
+				res.send('<form action="/" method="post" enctype="application/x-www-form-urlencoded"><input type="submit"><textarea name="config" style="width:100%;height:500px;"></textarea></form>');
+			});
+
+			app.post('/', function (req, res) {
+				logger.log('info', 'https: new configuration!');
+				config.set(JSON.parse(req.body.config));
+				extensions.generate(function () {
+					res.send('Done!');
+					serverDaemonNote();
+				});
+			});
+
+			var server = https.createServer({
+				key: fs.readFileSync(path.normalize(argv.sslPrefix + '.key')),
+				cert: fs.readFileSync(path.normalize(argv.sslPrefix + '.crt'))
+			}, app).listen(argv.httpsPort);
+		}
 	}
-});
+}
