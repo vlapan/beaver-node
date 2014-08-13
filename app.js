631558499ecc84013f7bb6d77a946c5395e83b9e
vlapan <vlapan@vlapan.com>
2014-08-13 15:00:48 +0400

    updates

--- app.js
+++ app.js
@@ -116,8 +116,8 @@ function callback() {
 				if (req.body.forward && fs.existsSync('/usr/local/etc/beaver/bobot.auth')) {
 					var auth = fs.readFileSync('/usr/local/etc/beaver/bobot.auth');
 					auth = auth.toString().replace(/\s+/gi, '');
-					Object.keys(config.vms).filter(function(item) {
-						return item.router;
+					Object.keys(config.vms).filter(function(key) {
+						return config.vms[key].router;
 					}).forEach(function (key) {
 						console.log("curl 'https://" + key + ":" + argv.httpsPort + "/' -u " + auth + " -H 'Content-Type: application/x-www-form-urlencoded' --compressed -k");
 						exec("curl 'https://" + key + ":" + argv.httpsPort + "/' -u " + auth + " -H 'Content-Type: application/x-www-form-urlencoded' --data '" + qs.stringify(req.body) + "' --compressed -k", function (error, stdout, stderr) {
