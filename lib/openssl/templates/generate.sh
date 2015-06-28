d2863af236165518b9eb79dabed48839831f49a8
vlapan <vlapan@vlapan.com>
2015-06-28 23:59:34 +0300

    ssl certs presets

--- lib/openssl/templates/generate.sh
+++ lib/openssl/templates/generate.sh
@@ -7,12 +7,15 @@ ROUTE=%{route}
 ROOTNAME=%{prefixRoot}
 SUBJECTPREFIX=%{subjectPrefix}
 TEMPPASS=%{tempPass}
+KEYSIZE=%{keySize}
+SIGNATUREALGORITHM=%{signatureAlgorithm}
+EXPIRATIONDAYS=%{expirationDays}
 
 [ -f ${NAME}.key.original ] || {
-	openssl genrsa -des3 -passout ${TEMPPASS} -out ${NAME}.key.original 1024
+	openssl genrsa -des3 -passout ${TEMPPASS} -out ${NAME}.key.original ${KEYSIZE}
 	openssl rsa -passin ${TEMPPASS} -in ${NAME}.key.original -out ${NAME}.key
 }
 [ -f ${NAME}.csr ] || openssl req -new -batch -subj "${SUBJECTPREFIX}${ROUTE}" -key ${NAME}.key -out ${NAME}.csr
-[ -f ${NAME}.raw.crt ] || openssl x509 -req -days 365 -in ${NAME}.csr -CA ${ROOTNAME}.crt -CAkey ${ROOTNAME}.key -CAcreateserial -out ${NAME}.raw.crt
+[ -f ${NAME}.raw.crt ] || openssl x509 -req -${SIGNATUREALGORITHM} -days ${EXPIRATIONDAYS} -in ${NAME}.csr -CA ${ROOTNAME}.crt -CAkey ${ROOTNAME}.key -CAcreateserial -out ${NAME}.raw.crt
 [ -f ${NAME}.crt ] || cat ${NAME}.raw.crt ${ROOTNAME}.crt > ${NAME}.crt
 [ -f ${NAME}.crt ] && openssl x509 -in ${NAME}.crt -out ${NAME}.pem -outform PEM
