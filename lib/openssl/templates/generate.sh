5f0e6074cfece2e98461a5140761e687d2e4ce1e
vlapan <vlapan@vlapan.com>
2015-07-12 02:01:23 +0300

    fix

--- lib/openssl/templates/generate.sh
+++ lib/openssl/templates/generate.sh
@@ -19,11 +19,11 @@ SERIAL=%{serial}
 }
 [ -f ${NAME}.csr ] || openssl req -new -batch -subj "${SUBJECTPREFIX}${ROUTE}" -key ${NAME}.key -out ${NAME}.csr
 [ -f ${NAME}.raw.crt ] || openssl x509 -req -${SIGNATUREALGORITHM} -days ${EXPIRATIONDAYS} -in ${NAME}.csr -CA ${ROOTNAME}.crt -CAkey ${ROOTNAME}.key -set_serial ${SERIAL} -out ${NAME}.raw.crt
-[ -f ${NAME}.crt ] || (
+if [ ! -f ${NAME}.crt ]; then
 	cat ${NAME}.raw.crt ${ROOTNAME}.crt > ${NAME}.crt
-	if [ "${INCLUDEROOTCA}" = "true" && -f ${ROOTNAME}.ca ]; then
+	if [ "${INCLUDEROOTCA}" = "true" ] && [ -f ${ROOTNAME}.ca ]; then
 		cat ${ROOTNAME}.ca >> ${NAME}.crt
 	fi
-)
+fi
 
 #[ -f ${NAME}.crt ] && openssl x509 -in ${NAME}.crt -out ${NAME}.pem -outform PEM
