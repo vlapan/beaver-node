7481017f1709906681d5dd9fb7911b5dbd29d706
vlapan <vlapan@vlapan.com>
2017-03-26 04:20:31 +0300

    up

--- lib/openssl/templates/generate.sh
+++ lib/openssl/templates/generate.sh
@@ -18,9 +18,9 @@ SERIAL=%{serial}
 	openssl rsa -passin ${TEMPPASS} -in ${NAME}.key.original -out ${NAME}.key
 }
 [ -f ${NAME}.csr ] || openssl req -new -batch -subj "${SUBJECTPREFIX}${ROUTE}" -key ${NAME}.key -out ${NAME}.csr
-[ -f ${NAME}.raw.crt ] || openssl x509 -req -${SIGNATUREALGORITHM} -days ${EXPIRATIONDAYS} -in ${NAME}.csr -CA ${ROOTNAME}.crt -CAkey ${ROOTNAME}.key -set_serial ${SERIAL} -out ${NAME}.raw.crt
+[ -f ${NAME}.raw-crt ] || openssl x509 -req -${SIGNATUREALGORITHM} -days ${EXPIRATIONDAYS} -in ${NAME}.csr -CA ${ROOTNAME}.ca-crt -CAkey ${ROOTNAME}.ca-key -set_serial ${SERIAL} -out ${NAME}.raw-crt
 if [ ! -f ${NAME}.crt ]; then
-	cat ${NAME}.raw.crt ${ROOTNAME}.crt > ${NAME}.crt
+	cat ${NAME}.raw-crt ${ROOTNAME}.ca-crt > ${NAME}.crt
 	if [ "${INCLUDEROOTCA}" = "true" ] && [ -f ${ROOTNAME}.ca ]; then
 		cat ${ROOTNAME}.ca >> ${NAME}.crt
 	fi
