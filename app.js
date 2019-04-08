a8490357080b057998fa19137c866658c4fdaad6
vlapan <vlapan@vlapan.com>
2019-04-08 23:25:06 +0300

    updates

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
-"use strict";
+const argv = require(`${__dirname}/lib/argv`);
+const logger = require(`${__dirname}/lib/logger`);
 
-var argv = require(__dirname + '/lib/argv');
-var logger = require(__dirname + '/lib/logger');
-
-var config = require(__dirname + '/lib/configuration');
-var extensions = require(__dirname + '/lib/extensions');
+const config = require(`${__dirname}/lib/configuration`);
+const extensions = require(`${__dirname}/lib/extensions`);
 
 function fileDaemonNote() {
-	logger.log('info', 'input: daemon mode, watching config file "' + argv.input + '"!');
+    logger.log('info', `input: daemon mode, watching config file "${argv.input}"!`);
 }
 
 function daemonStart(err) {
-	if (!argv.daemon || err) {
-		process.exit();
-		return;
-	}
+    if (!argv.daemon || err) {
+        process.exit();
+        return;
+    }
 
-	if (argv.input) {
-		fileDaemonNote();
-		config.watch(function () {
-			extensions.generate(fileDaemonNote);
-		});
-	}
+    if (argv.input) {
+        fileDaemonNote();
+        config.watch(() => {
+            extensions.generate(fileDaemonNote);
+        });
+    }
 
-	require(__dirname + '/lib/https');
+    require(`${__dirname}/lib/https`);
 
-	var extensionsEnabled = argv.e.split(',');
-	if (~extensionsEnabled.indexOf('monitor')) {
-		var Overseer = require('./lib/overseer/overseer');
-		var overseer = new Overseer({
-			data: argv.home + '/monitor.json',
-			result: argv.home + '/monitor-result.txt',
-			interval: 10000,
-			tcpTimeout: 5000,
-			webTimeout: 10000
-		});
-		overseer.start();
-	}
+    const extensionsEnabled = argv.e.split(',');
+    if (~extensionsEnabled.indexOf('monitor') && !argv.disableOverseer) {
+        const Overseer = require('./lib/overseer/overseer');
+        const overseer = new Overseer({
+            data: `${argv.home}/monitor.json`,
+            result: `${argv.home}/monitor-result.txt`,
+            interval: 10000,
+            tcpTimeout: 5000,
+            webTimeout: 10000,
+        });
+        overseer.start();
+    }
 
-	if (argv.discover) {
-		require(__dirname + '/lib/discovery');
-	}
+    if (argv.discover) {
+        require(`${__dirname}/lib/discovery`);
+    }
 }
 
 if (argv.input) {
-	extensions.generate(daemonStart);
+    extensions.generate(daemonStart);
 } else if (argv.daemon) {
-	daemonStart();
+    daemonStart();
 }
