de818cadf400fd334a039ff9a1a026c6e33bdef5
vlapan <vlapan@vlapan.com>
2019-08-24 19:47:17 +0300

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,36 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
-  ],
-  "rules": {
-    "max-len": ["error", 999, 4],
-    "indent": ["error", 4],
-    "no-console": "off",
-    "no-continue": "off",
-    "no-underscore-dangle": "off",
-    "no-bitwise": ["error", {
-      "int32Hint": true,
-      "allow": ["~"]
-    }]
-  }
+    parser: 'babel-eslint',
+    extends: [
+        'airbnb-base',
+        'plugin:flowtype/recommended',
+        'plugin:css-modules/recommended',
+        'prettier',
+        'prettier/flowtype',
+    ],
+    plugins: [
+        'import',
+        'flowtype',
+        'css-modules',
+        'prettier',
+    ],
+    rules: {
+        'max-len': ['error', 999, 4],
+        'indent': ['error', 4],
+        'func-names': ['error', 'never'],
+        'no-console': 'off',
+        'no-continue': 'off',
+        'no-underscore-dangle': 'off',
+        'no-bitwise': ['error', {
+            'int32Hint': true,
+            'allow': ['~'],
+        }],
+        'no-restricted-syntax': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                moduleDirectory: ['node_modules', 'src'],
+            },
+        },
+    },
 };
--- build-view.js
+++
@@ -1,7 +0,0 @@
-const puglatizer = require('puglatizer');
-
-puglatizer(`./lib/https/views/dynamic`, `./lib/https/public/js/view-compiled.js`, {
-    compileDebug: false,
-    cache: false,
-    debug: false,
-});
