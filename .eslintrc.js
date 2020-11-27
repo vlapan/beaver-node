8a733bdf28b3ac7038eaf9e45133ec258359c81b
vlapan <vlapan@vlapan.com>
2020-11-27 18:07:49 +0300

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,46 @@
 module.exports = {
     extends: [
-        'airbnb-base',
-        "eslint:recommended",
-        "plugin:node/recommended",
-        'plugin:css-modules/recommended',
-        // 'plugin:security/recommended',
-        'prettier',
-        'prettier/flowtype',
+        'eslint:recommended',
+        'standard',
+        'plugin:node/recommended',
+        'plugin:promise/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
     ],
     plugins: [
+        'standard',
+        'node',
+        'promise',
         'import',
-        'flowtype',
-        'css-modules',
-        'prettier',
     ],
-    parserOptions: {
-        "ecmaVersion": 2020
+    env: {
+        node: true,
+        browser: true,
     },
     rules: {
-        'semi': [2, "always", { "omitLastInOneLineBlock": true }],
+        indent: ['error', 4], // ["error", 4, { "SwitchCase": 1 }],
+        semi: [2, 'always', {
+            omitLastInOneLineBlock: true,
+        }],
+        'comma-dangle': ['error', 'always-multiline'],
         'max-len': ['error', 999, 4],
-        "indent": ["error", 4], // ["error", 4, { "SwitchCase": 1 }],
+        'space-before-function-paren': ['error', {
+            anonymous: 'always',
+            named: 'never',
+            asyncArrow: 'always',
+        }],
         'func-names': ['error', 'never'],
         'no-console': 'off',
         'no-continue': 'off',
         'no-underscore-dangle': 'off',
         'no-bitwise': ['error', {
-            'int32Hint': true,
-            'allow': ['~'],
+            int32Hint: true,
+            allow: ['~'],
         }],
         'no-restricted-syntax': 'off',
         'no-plusplus': 'off',
         'no-await-in-loop': 'off',
+        'multiline-ternary': ['error', 'never'],
     },
     settings: {
         'import/resolver': {
@@ -39,5 +48,10 @@ module.exports = {
                 moduleDirectory: ['node_modules', 'lib', 'src'],
             },
         },
+    },
+    globals: {
+        yaumnrc: 'readonly',
+        explainer: 'readonly',
+        ace: 'readonly',
     },
 };
