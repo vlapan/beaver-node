68f49641eb3de9f86d06146ee2a72453107b5b6a
vlapan <vlapan@vlapan.com>
2021-05-14 08:03:43 +0300

    deps update, version bump

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,9 @@ module.exports = {
         node: true,
         browser: true,
     },
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
     overrides: [{
         files: ['**/*.json'],
     }],
