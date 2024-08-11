749bdfe84169a8933009e6caeeb77c7461d4a162
vlapan <vlapan@vlapan.com>
2024-08-11 13:09:35 +0300

    style: format ".babelrc.js"

--- .babelrc.js
+++ .babelrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env', {
-        targets: {
-          node: 'current',
-        },
-      },
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+        '@babel/preset-stage-2',
+        '@babel/preset-flow',
     ],
-    '@babel/preset-stage-2',
-    '@babel/preset-flow',
-  ],
-  ignore: ['node_modules', 'build'],
-  "plugins": [
-    "transform-decorators-legacy",
-    "syntax-async-functions",
-    "transform-regenerator"
-  ],
-  "sourceMaps": true,
-  "retainLines": true,
-  "comments": false,
+    ignore: ['node_modules', 'build'],
+    plugins: ['transform-decorators-legacy', 'syntax-async-functions', 'transform-regenerator'],
+    sourceMaps: true,
+    retainLines: true,
+    comments: false,
 };
