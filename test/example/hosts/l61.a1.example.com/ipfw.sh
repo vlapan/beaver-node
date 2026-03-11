0ac087e8daef693ff04e391dc3dee164b92f81c4
vlapan <vlapan@vlapan.com>
2026-03-11 15:01:11 +0300

    feat(ipfw): split rules

--- test/example/hosts/l61.a1.example.com/ipfw.sh
+++ test/example/hosts/l61.a1.example.com/ipfw.sh
@@ -150,39 +150,79 @@ $fw nat 1 config ip 10.20.20.20 unreg_only \
     set 2 table service-port-forward-access-7a40909f-tmp swap service-port-forward-access-7a40909f
     set 2 table service-port-forward-access-7a40909f-tmp destroy
 
-
-    add 504 set 2 allow ip from any to me dst-port 80 in // http: allow all 
-    add 504 set 2 allow ip from any to me dst-port 443 in // http: allow all
-
-    add 504 set 2 allow tcp from any to me dst-port 22 in // ssh: allow all 
-    add 504 set 2 allow tcp from any to me dst-port 27 in // ssh: allow all
-
-    add 504 set 2 count tcp from any to me dst-port 53 in // tcp dns
-    add 504 set 2 allow ip from any to me dst-port 53 in // dns
-
-    add 504 set 2 allow ip from any to me dst-port 123 in // ntp: allow all
-
-    add 504 set 2 allow ip from table(tinc-acl-table) to me dst-port 655 in // tinc: allow specific
-    add 504 set 2 deny ip from any to me dst-port 655 in // tinc: deny others
-
-    add 504 set 2 allow ip from table(beaver-acl-table) to me dst-port 8443 in // beaver-api: allow specific
-    add 504 set 2 deny ip from any to me dst-port 8443 in // beaver-api: deny others
+    add 00999 set 2 skipto 10000 ip4 from any to any in
+    add 00999 set 2 skipto 20000 ip4 from any to any out
+    add 00999 set 2 skipto 30000 ip6 from any to any in
+    add 00999 set 2 skipto 40000 ip6 from any to any out
+    add 00999 set 2 allow ip4 from any to any // why any traffic here?
+    add 00999 set 2 allow ip6 from any to any // why any traffic here?
 
 
-    add 506 set 2 allow ip from me to not me src-port 80 out // http out 
-    add 506 set 2 allow ip from me to not me src-port 443 out // http out
-    add 506 set 2 allow tcp from me to not me src-port 22 out // ssh out 
-    add 506 set 2 allow tcp from me to not me src-port 27 out // ssh out
-    add 506 set 2 count tcp from me to not me src-port 53 out // dns out
-    add 506 set 2 allow ip from me to not me src-port 53 out // dns out
-    add 506 set 2 allow ip from me to not me src-port 123 out // ntp out
-    add 506 set 2 allow ip from me to not me src-port 655 out // tinc out
-    add 506 set 2 allow ip from me to not me src-port 8443 out // beaver-api out
+    add 19999 set 2 allow ip4 from any to me  in // dynamic v4 in
+    add 19999 set 2 allow ip  from any to any in // why any traffic here?
+
+    add 29999 set 2 allow ip4 from me  to not me  out // dynamic v4 out
+    add 29999 set 2 allow ip  from me  to any out // why any traffic here?
+
+    add 39999 set 2 allow ip6 from any to me6 in // dynamic v6 in
+    add 39999 set 2 allow ip  from any to any in // why any traffic here?
+
+    add 49999 set 2 allow ip6 from me  to not me  out // dynamic v6 out
+    add 49999 set 2 allow ip  from me  to any out // why any traffic here?
 
 
-    add 508 set 2 count ip6 from me to not me out // ipv6 of all
-    add 508 set 2 count udp from me to not me out // udp of all
-    add 508 set 2 allow ip from me to not me out // all outgoing blindly allowed
+    add 11010 set 2 allow tcp from any to me  dst-port 443 in // http: allow all 
+    add 11011 set 2 allow tcp from any to me  dst-port 80 in // http: allow all
+    add 11010 set 2 allow udp from any to me  dst-port 443 in // http: allow all 
+    add 11011 set 2 allow udp from any to me  dst-port 80 in // http: allow all
+    add 31010 set 2 allow tcp from any to me6 dst-port 443 in // http: allow all 
+    add 31011 set 2 allow tcp from any to me6 dst-port 80 in // http: allow all
+    add 31010 set 2 allow udp from any to me6 dst-port 443 in // http: allow all 
+    add 31011 set 2 allow udp from any to me6 dst-port 80 in // http: allow all
+
+    add 11020 set 2 allow tcp from any to me  dst-port 22 in // ssh: allow all 
+    add 11021 set 2 allow tcp from any to me  dst-port 27 in // ssh: allow all
+    add 31020 set 2 allow tcp from any to me6 dst-port 22 in // ssh: allow all 
+    add 31021 set 2 allow tcp from any to me6 dst-port 27 in // ssh: allow all
+
+    add 11030 set 2 allow tcp from any to me  dst-port 53 in // tcp dns
+    add 11030 set 2 allow udp from any to me  dst-port 53 in // dns
+    add 31030 set 2 count tcp from any to me6 dst-port 53 in // tcp dns
+    add 31030 set 2 allow udp from any to me6 dst-port 53 in // dns
+
+    add 11031 set 2 allow udp from any to me  dst-port 123 in // ntp: allow all
+    add 31031 set 2 allow udp from any to me6 dst-port 123 in // ntp: allow all
+
+    add 11032 set 2 allow ip4 from table(tinc-acl-table) to me  dst-port 655 in // tinc: allow specific
+    add 11032 set 2 deny  ip4 from any to me  dst-port 655 in // tinc: deny others
+    add 31032 set 2 allow ip6 from table(tinc-acl-table) to me6 dst-port 655 in // tinc: allow specific
+    add 31032 set 2 deny  ip6 from any to me6 dst-port 655 in // tinc: deny others
+
+    add 11033 set 2 allow ip4 from table(beaver-acl-table) to me  dst-port 8443 in // beaver-api: allow specific
+    add 11033 set 2 deny  ip4 from any to me  dst-port 8443 in // beaver-api: deny others
+    add 31033 set 2 allow ip6 from table(beaver-acl-table) to me6 dst-port 8443 in // beaver-api: allow specific
+    add 31033 set 2 deny  ip6 from any to me6 dst-port 8443 in // beaver-api: deny others
+
+
+    add 21010 set 2 allow ip4 from me to not me  src-port 443 out // http out 
+    add 21011 set 2 allow ip4 from me to not me  src-port 80 out // http out
+    add 41010 set 2 allow ip6 from me to not me6 src-port 443 out // http out 
+    add 41011 set 2 allow ip6 from me to not me6 src-port 80 out // http out
+    add 21020 set 2 allow tcp from me to not me  src-port 22 out // ssh out 
+    add 21021 set 2 allow tcp from me to not me  src-port 27 out // ssh out
+    add 41020 set 2 allow tcp from me to not me6 src-port 22 out // ssh out 
+    add 41021 set 2 allow tcp from me to not me6 src-port 27 out // ssh out
+    add 21030 set 2 allow tcp from me to not me  src-port 53 out // dns out
+    add 41030 set 2 allow tcp from me to not me6 src-port 53 out // dns out
+    add 21030 set 2 allow udp from me to not me  src-port 53 out // dns out
+    add 41030 set 2 allow udp from me to not me6 src-port 53 out // dns out
+    add 21031 set 2 allow udp from me to not me  src-port 123 out // ntp out
+    add 41031 set 2 allow udp from me to not me6 src-port 123 out // ntp out
+    add 21032 set 2 allow ip4 from me to not me  src-port 655 out // tinc out
+    add 41032 set 2 allow ip6 from me to not me6 src-port 655 out // tinc out
+    add 21033 set 2 allow ip4 from me to not me  src-port 8443 out // beaver-api out
+    add 41033 set 2 allow ip6 from me to not me6 src-port 8443 out // beaver-api out
+
 
     set 2 table service-port-forward-target create missing type flow:proto,dst-ip,dst-port valtype tag
     set 2 table service-port-forward-target-tmp create or-flush type flow:proto,dst-ip,dst-port valtype tag
@@ -201,11 +241,11 @@ $fw nat 1 config ip 10.20.20.20 unreg_only \
     set 2 table service-port-forward-target swap service-port-forward-target-tmp
     set 2 table service-port-forward-target-tmp destroy
     
-    add 510 set 2 skipto 511 tag tablearg ip from any to any flow table(service-port-forward-target) in // goto access check rule
-    add 510 set 2 skipto 520 ip from any to any in // skip to next stage
+    add 15510 set 2 skipto 15511 tag tablearg ip from any to any flow table(service-port-forward-target) in // goto access check rule
+    add 15510 set 2 skipto 15520 ip from any to any in // skip to next stage
     
-    add 511 set 2 drop ip from not table(service-port-forward-access-ff5eec00) to any in tagged 1001 // source to target
-    add 511 set 2 drop ip from not table(service-port-forward-access-7a40909f) to any in tagged 1002 // source to target
+    add 15511 set 2 drop ip from not table(service-port-forward-access-ff5eec00) to any in tagged 1001 // source to target
+    add 15511 set 2 drop ip from not table(service-port-forward-access-7a40909f) to any in tagged 1002 // source to target
     
 
     set 2 table service-wan create missing type flow:dst-ip,dst-port
@@ -298,27 +338,25 @@ $fw nat 1 config ip 10.20.20.20 unreg_only \
     set 2 table service-lan swap service-lan-tmp
     set 2 table service-lan-tmp destroy
     
-    add 522 set 2 skipto 540 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to not me flow table(service-lan) // svc lan-2-wan
-    add 523 set 2 skipto 540 ip from any to 10.20.20.20 flow table(service-wan) // svc any-2-lan
+    add 15522 set 2 skipto 15540 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to not me flow table(service-lan) in // svc lan-2-wan
+    add 15523 set 2 skipto 15540 ip from any to 10.20.20.20 flow table(service-wan) in // svc any-2-lan
     
-    add 530 set 2 nat 2 tag 7 ip from any to 10.20.20.20 in // incoming nat, dynamic
-    add 531 set 2 nat 2 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to any out tagged 7 // hairpin, dynamic
-    add 532 set 2 nat 2 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to not 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 out // outgoing nat, dynamic
+    add 15530 set 2 nat 2 tag 7 ip from any to 10.20.20.20 in // incoming nat, dynamic
+    add 15535 set 2 skipto 15600 ip from any to any
+    add 25531 set 2 nat 2 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to any out tagged 7 // hairpin, dynamic
+    add 25532 set 2 nat 2 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to not 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 out // outgoing nat, dynamic
+    add 25535 set 2 skipto 25600 ip from any to any
     
-    add 535 set 2 skipto 550 ip from any to any
-    
-    add 540 set 2 nat 1 tag 7 ip from any to 10.20.20.20 in // incoming nat, service
-    add 541 set 2 nat 1 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to any out tagged 7 // hairpin, service
-    add 542 set 2 nat 1 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to not 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 out // outgoing nat, service
+    add 15540 set 2 nat 1 tag 7 ip from any to 10.20.20.20 in // incoming nat, service
+    add 25541 set 2 nat 1 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to any out tagged 7 // hairpin, service
+    add 25542 set 2 nat 1 ip from 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 to not 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 out // outgoing nat, service
 
 
-    add 800 set 2 deny icmp from me to table(tinc-tap-l6-hosts-local) icmptype 5 in // block redirects for tincd
+    add 15800 set 2 deny icmp from me to table(tinc-tap-l6-hosts-local) icmptype 5 in // block redirects for tincd
     
 
 
-    add 900 set 2 count ip6 from any to any // ipv6 of all
-    add 900 set 2 count ip from not me to me in // incoming of all
-    add 900 set 2 allow ip from any to any // all traffic blindly allowed
+    add 50000 set 2 allow ip from any to any // all traffic blindly allowed
 
     set swap 2 1
     set enable 1
